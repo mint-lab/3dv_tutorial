9fdb3c2e7e4f328218683e192ea91c4684b51954
Sunglok Choi <sunglok@hanmail.net>
2019-08-06 11:48:19 +0900

    Improve UI of 'simple_object_filtering'

--- src/simple_object_filtering.cpp
+++ src/simple_object_filtering.cpp
@@ -6,8 +6,6 @@ int main(void)
     double f = 810.5, cx = 480, cy = 270, theta = 18.7 * CV_PI / 180, L = 3.31;
     int backsub_history = 1000, area_thresh = 100;
     double backsub_thresh = 32, height_thresh = 1;
-    cv::Mat morph_kernel_small = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(3, 3));
-    cv::Mat morph_kernel_big = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 10));
 
     // Open a video and create MOG2 background subtractor
     cv::VideoCapture video;
@@ -15,6 +13,9 @@ int main(void)
     if (!video.open("data/daejeon.avi") || backsub.empty()) return -1;
 
     // Run foreground extraction and object detection
+    cv::Matx33d R_t(1, 0, 0, 0, cos(-theta), -sin(-theta), 0, sin(-theta), cos(-theta));
+    cv::Mat morph_kernel_small = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(3, 3));
+    cv::Mat morph_kernel_big = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 10));
     while (true)
     {
         // Grab an image from the video
@@ -53,18 +54,27 @@ int main(void)
         cv::cvtColor(fgmask, fgmask, cv::COLOR_GRAY2BGR);
         for (size_t i = 0; i < blobs.size(); i++)
         {
-            double phi = atan2(blobs[i].y + blobs[i].height - cy, f), phi_head = atan2(blobs[i].y - cy, f);
-            double Z = L / tan(phi + theta), Z_head = L / tan(phi_head + theta);
-            double H = (Z_head - Z) / Z_head * L;
+            cv::Vec3d p_foot = R_t * cv::Vec3d(blobs[i].x + blobs[i].width / 2 - cx, blobs[i].y + blobs[i].height - cy, f);
+            cv::Vec3d p_head = R_t * cv::Vec3d(blobs[i].x + blobs[i].width / 2 - cx, blobs[i].y - cy, f);
+            if (p_foot[1] <= 0 || p_head[1] <= 0) continue;
+            double Z_foot = p_foot[2] / p_foot[1] * L, x_foot = p_foot[0] / p_foot[1] * L;
+            double Z_head = p_head[2] / p_head[1] * L;
+            double H = (Z_head - Z_foot) / Z_head * L;
             cv::Scalar color = cv::Scalar(0, 255, 0);
             if (use_height_filter && H < height_thresh) color = cv::Scalar(127, 127, 255);
             cv::rectangle(image, blobs[i], color, 2);
             cv::rectangle(fgmask, blobs[i], color, 2);
-            cv::putText(image, cv::format("(D%.2f,H%.2f)", Z, H), blobs[i].tl() + cv::Point(-5, -15), cv::FONT_HERSHEY_DUPLEX, 0.5, color);
+            cv::putText(image, cv::format("(X:%.2f,Z:%.2f,H:%.2f)", x_foot, Z_foot, H), blobs[i].tl() + cv::Point(-5, -15), cv::FONT_HERSHEY_DUPLEX, 0.5, color);
         }
         cv::hconcat(image, fgmask, image);
         cv::imshow("3DV Tutorial: Simple Object Filtering", image);
-        if (cv::waitKey(1) == 27) break; // 'ESC' key: Exit
+        int key = cv::waitKey(1);
+        if (key == 27) break;       // 'ESC' key: Exit
+        else if (key == 32)         // 'Space' key: Pause
+        {
+            key = cv::waitKey(0);
+            if (key == 27) break;   // 'ESC' key: Exit
+        }
     }
 
     video.release();
