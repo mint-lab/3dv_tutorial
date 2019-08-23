dec5e366e66ccb654872b1e9afd9ea0ebdf4e30e
Sunglok Choi <sunglok@hanmail.net>
2019-08-23 17:12:44 +0900

    Move common SFM codes to 'sfm.hpp'

--- src/sfm.hpp
+++ src/sfm.hpp
@@ -4,6 +4,7 @@
 #include "opencv2/opencv.hpp"
 #include "ceres/ceres.h"
 #include "ceres/rotation.h"
+#include <unordered_map>
 
 // Reprojection error for bundle adjustment
 // - Ref. http://ceres-solver.org/nnls_tutorial.html#bundle-adjustment
@@ -47,6 +48,72 @@ struct ReprojectionErrorSnavely
 
 private:
     const cv::Point2d x;
+};
+
+class SFM
+{
+public:
+
+    typedef cv::Vec<double, 11> Vec11d;
+
+    typedef std::unordered_map<uint, uint> VisibilityGraph;
+
+    static inline uint genKey(uint img_idx, uint pt_idx) { return ((img_idx << 16) + pt_idx); }
+
+    static inline uint getImIdx(uint key) { return ((key >> 16) & 0xFFFF); }
+
+    static inline uint getPtIdx(uint key) { return (key & 0xFFFF); }
+
+    static bool addCostFunc(ceres::Problem& problem, const std::vector<cv::Point3d>& Xs, const std::vector<std::vector<cv::KeyPoint>>& xs, const std::vector<Vec11d>& views, const std::unordered_map<uint, uint>& visibility, double loss_width = 4)
+    {
+        for (auto visible = visibility.begin(); visible != visibility.end(); visible++)
+        {
+            int img_idx = getImIdx(visible->first), pt_idx = getPtIdx(visible->first);
+            const cv::Point2d& x = xs[img_idx][pt_idx].pt;
+            ceres::CostFunction* cost_func = ReprojectionErrorSnavely::create(x);
+            ceres::LossFunction* loss_func = NULL;
+            if (loss_width > 0) loss_func = new ceres::CauchyLoss(loss_width);
+            double* view = (double*)(&(views[img_idx]));
+            double* X = (double*)(&(Xs[visible->second]));
+            problem.AddResidualBlock(cost_func, loss_func, view, X);
+        }
+        return true;
+    }
+
+    static int markNoisyPoints(std::vector<cv::Point3d>& Xs, const std::vector<std::vector<cv::KeyPoint>>& xs, const std::vector<Vec11d>& views, const std::unordered_map<uint, uint>& visibility, double reproj_error2 = 4)
+    {
+        if (reproj_error2 <= 0) return -1;
+
+        int n_mark = 0;
+        for (auto visible = visibility.begin(); visible != visibility.end(); visible++)
+        {
+            cv::Point3d& X = Xs[visible->second];
+            if (X.z < 0) continue;
+            int img_idx = getImIdx(visible->first), pt_idx = getPtIdx(visible->first);
+            const cv::Point2d& x = xs[img_idx][pt_idx].pt;
+            const Vec11d& view = views[img_idx];
+
+            // Project the given 'X'
+            cv::Vec3d rvec(view[0], view[1], view[2]);
+            cv::Matx33d R;
+            cv::Rodrigues(rvec, R);
+            cv::Point3d X_p = R * X + cv::Point3d(view[3], view[4], view[5]);
+            const double &f = view[6], &k1 = view[9], &k2 = view[10];
+            cv::Point2d x_n(X_p.x / X_p.z, X_p.y / X_p.z), c(view[7], view[8]);
+            double r2 = x_n.x * x_n.x + x_n.y * x_n.y;
+            double radial_distort = 1 + r2 * (k1 + k2 * r2);
+            cv::Point2d x_p = f * radial_distort * x_n + c;
+
+            // Calculate distance between 'x' and 'x_p'
+            cv::Point2d d = x - x_p;
+            if (d.x * d.x + d.y * d.y > reproj_error2)
+            {
+                X.z *= -1;
+                n_mark++;
+            }
+        }
+        return n_mark;
+    }
 };
 
 #endif // End of '__SFM__'
--- src/sfm_global.cpp
+++ src/sfm_global.cpp
@@ -1,22 +1,17 @@
 #include "sfm.hpp"
-#include <unordered_map>
-
-#define MAKE_KEY(img_idx, pt_idx)       ((uint(img_idx) << 16) + pt_idx)
-#define GET_IMG_IDX(key)                ((key >> 16) & 0xFFFF)
-#define GET_PT_IDX(key)                 (key & 0xFFFF)
 
 int main()
 {
     const char* input = "data/relief/%02d.jpg";
-    double img_resize = 0.25, f_init = 500, Z_init = 5, Z_limit = 10;
-    size_t min_inlier_num = 200;
-    bool show_match = false;
+    double img_resize = 0.25, f_init = 500, Z_init = 2, Z_limit = 10, ba_loss_width = 9; // Negative 'loss_width' makes BA not to use a loss function.
+    size_t min_inlier_num = 500, ba_num_iter = 200; // Negative 'ba_num_iter' uses the default value for BA minimization
+    bool use_homography = true, show_match = true;
 
     // Load images and extract features
     cv::VideoCapture video;
     if (!video.open(input)) return -1;
     cv::Ptr<cv::FeatureDetector> fdetector = cv::BRISK::create();
-    std::vector<std::vector<cv::KeyPoint> > img_keypoint;
+    std::vector<std::vector<cv::KeyPoint>> img_keypoint;
     std::vector<cv::Mat> img_set, img_descriptor;
     while (true)
     {
@@ -24,6 +19,7 @@ int main()
         video >> image;
         if (image.empty()) break;
         if (img_resize != 1) cv::resize(image, image, cv::Size(), img_resize, img_resize);
+
         std::vector<cv::KeyPoint> keypoint;
         cv::Mat descriptor;
         fdetector->detectAndCompute(image, cv::Mat(), keypoint, descriptor);
@@ -34,14 +30,15 @@ int main()
     if (img_set.size() < 2) return -1;
     cv::Point2d img_center(img_set.front().cols / 2, img_set.front().rows / 2);
 
-    // Match features all over the image pairs and find inliers
+    // Match features and find good matches
     cv::Ptr<cv::DescriptorMatcher> fmatcher = cv::DescriptorMatcher::create("BruteForce-Hamming");
-    std::vector<std::pair<int, int> > match_pair;           // The viewing graph (image pairs)
-    std::vector<std::vector<cv::DMatch> > match_inlier;     // The viewing graph (indices of inlier matches)
+    std::vector<std::pair<int, int> > match_pair;       // The good match pairs (image pairs)
+    std::vector<std::vector<cv::DMatch> > match_inlier; // The good match pairs (inlier point pairs)
     for (size_t i = 0; i < img_set.size(); i++)
     {
         for (size_t j = i + 1; j < img_set.size(); j++)
         {
+            // Match features of two image pair (i, j) and find their inliers
             std::vector<cv::DMatch> match, inlier;
             fmatcher->match(img_descriptor[i], img_descriptor[j], match);
             std::vector<cv::Point2d> src, dst;
@@ -51,18 +48,21 @@ int main()
                 dst.push_back(img_keypoint[j][itr->trainIdx].pt);
             }
             cv::Mat inlier_mask;
-            cv::findFundamentalMat(src, dst, inlier_mask, cv::RANSAC);
+            if (use_homography) cv::findHomography(src, dst, inlier_mask, cv::RANSAC);
+            else                cv::findFundamentalMat(src, dst, inlier_mask, cv::RANSAC);
             for (int k = 0; k < inlier_mask.rows; k++)
                 if (inlier_mask.at<uchar>(k)) inlier.push_back(match[k]);
             printf("3DV Tutorial: Image %d - %d are matched (%d / %d).\n", i, j, inlier.size(), match.size());
-            if (inlier.size() < min_inlier_num) continue; // The threshold for putting into the viewing graph
+
+            // Determine whether the image pair is good or not
+            if (inlier.size() < min_inlier_num) continue;
             printf("3DV Tutorial: Image %d - %d are selected.\n", i, j);
             match_pair.push_back(std::pair<int, int>(i, j));
             match_inlier.push_back(inlier);
             if (show_match)
             {
                 cv::Mat match_image;
-                cv::drawMatches(img_set[i], img_keypoint[i], img_set[j], img_keypoint[j], match, match_image, cv::Vec3b(0, 0, 255), cv::Vec3b(0, 127, 0), inlier_mask);
+                cv::drawMatches(img_set[i], img_keypoint[i], img_set[j], img_keypoint[j], match, match_image, cv::Scalar::all(-1), cv::Scalar::all(-1), inlier_mask);
                 cv::imshow("3DV Tutorial: Global Structure-from-Motion", match_image);
                 cv::waitKey();
             }
@@ -71,28 +71,28 @@ int main()
     if (match_pair.size() < 1) return -1;
 
     // Initialize camera views
-    typedef cv::Vec<double, 11> Vec11d;
-    std::vector<Vec11d> views(img_set.size(), Vec11d(0, 0, 0, 0, 0, 0, f_init, img_center.x, img_center.y));
+    std::vector<SFM::Vec11d> views(img_set.size(), SFM::Vec11d(0, 0, 0, 0, 0, 0, f_init, img_center.x, img_center.y));
 
     // Initialize 3D points and build a visibility graph
     std::vector<cv::Point3d> Xs;
-    std::unordered_map<uint, uint> xs_visited;
+    SFM::VisibilityGraph xs_visited;
     for (size_t m = 0; m < match_pair.size(); m++)
     {
         for (size_t in = 0; in < match_inlier[m].size(); in++)
         {
-            const int &img1 = match_pair[m].first, &img2 = match_pair[m].second, &x1_idx = match_inlier[m][in].queryIdx, &x2_idx = match_inlier[m][in].trainIdx;
-            const uint key1 = MAKE_KEY(img1, x1_idx), key2 = MAKE_KEY(img2, x2_idx);
+            const int &img1 = match_pair[m].first, &img2 = match_pair[m].second;
+            const int &x1_idx = match_inlier[m][in].queryIdx, &x2_idx = match_inlier[m][in].trainIdx;
+            const uint key1 = SFM::genKey(img1, x1_idx), key2 = SFM::genKey(img2, x2_idx);
             auto visit1 = xs_visited.find(key1), visit2 = xs_visited.find(key2);
             if (visit1 != xs_visited.end() && visit2 != xs_visited.end())
             {
+                // Remove previous observations if they are not consistent
                 if (visit1->second != visit2->second)
                 {
                     xs_visited.erase(visit1);
                     xs_visited.erase(visit2);
                 }
-                // Skip if two observations are already visited
-                continue;
+                continue; // Skip if two observations are already visited
             }
 
             uint X_idx = 0;
@@ -111,36 +111,27 @@ int main()
 
     // Run bundle adjustment
     ceres::Problem ba;
-    for (auto visit = xs_visited.begin(); visit != xs_visited.end(); visit++)
-    {
-        int img_idx = GET_IMG_IDX(visit->first), pt_idx = GET_PT_IDX(visit->first);
-        const cv::Point2d& x = img_keypoint[img_idx][pt_idx].pt;
-        ceres::CostFunction* cost_func = ReprojectionErrorSnavely::create(x);
-        double* view = (double*)(&(views[img_idx]));
-        double* X = (double*)(&(Xs[visit->second]));
-        ba.AddResidualBlock(cost_func, NULL, view, X);
-    }
+    SFM::addCostFunc(ba, Xs, img_keypoint, views, xs_visited, ba_loss_width);
     ceres::Solver::Options options;
     options.linear_solver_type = ceres::ITERATIVE_SCHUR;
-    options.max_num_iterations = 200;
+    if (ba_num_iter > 0) options.max_num_iterations = ba_num_iter;
     options.num_threads = 8;
     options.minimizer_progress_to_stdout = true;
     ceres::Solver::Summary summary;
     ceres::Solve(options, &ba, &summary);
+    int n_mark = SFM::markNoisyPoints(Xs, img_keypoint, views, xs_visited, ba_loss_width);
     std::cout << summary.FullReport() << std::endl;
 
     // Store the 3D points to an XYZ file
     FILE* fpts = fopen("sfm_global(point).xyz", "wt");
     if (fpts == NULL) return -1;
-    int Xs_reject = 0;
     for (size_t i = 0; i < Xs.size(); i++)
     {
         if (Xs[i].z > 0 && Xs[i].z < Z_limit)
             fprintf(fpts, "%f %f %f\n", Xs[i].x, Xs[i].y, Xs[i].z);
-        else Xs_reject++;
     }
     fclose(fpts);
-    printf("3DV Tutorial: # of 3D points = %d (# of rejected = %d).\n", Xs.size(), Xs_reject);
+    printf("3DV Tutorial: # of 3D points = %d (# of rejected = %d).\n", Xs.size(), n_mark);
 
     // Store the camera poses to an XYZ file 
     FILE* fcam = fopen("sfm_global(camera).xyz", "wt");
