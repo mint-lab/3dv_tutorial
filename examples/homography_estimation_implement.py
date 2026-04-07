9e994f009ceef011fa98cc6ffda1bdff610d97b6
Sunglok Choi <sunglok@hanmail.net>
2026-04-07 15:42:56 +0900

    Fix typos in slides

--- examples/homography_estimation_implement.py
+++ examples/homography_estimation_implement.py
@@ -3,7 +3,7 @@ import cv2 as cv
 
 def getPerspectiveTransform(src, dst):
     if len(src) == len(dst):
-        # Make homogeneous coordiates if necessary
+        # Make homogeneous coordinates if necessary
         if src.shape[1] == 2:
             src = np.hstack((src, np.ones((len(src), 1), dtype=src.dtype)))
         if dst.shape[1] == 2:
--- examples/triangulation_implement.py
+++ examples/triangulation_implement.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     pts1 = np.loadtxt('../data/image_formation1.xyz')[:,:2]
     output_file = 'triangulation_implement.xyz'
 
-    # Estimate relative pose of two view
+    # Estimate the relative pose of two view
     F, _ = cv.findFundamentalMat(pts0, pts1, cv.FM_8POINT)
     K = np.array([[f, 0, cx], [0, f, cy], [0, 0, 1]])
     E = K.T @ F @ K
